f19427d46b0b44a6906b9e750cc0b789fe7e94f6
yb55 <yifanbao522@gmail.com>
2021-08-03 18:52:52 +1200

    4.10 ok

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,6 +2,35 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'HTML is easy',
+        author: 'bill',
+        url: 'www.google.com',
+        likes: 2
+    },
+    {
+        title: 'HTML is hard',
+        author: 'bill_bb',
+        url: 'www.nvc.com',
+        likes: 22
+    },
+  ]
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 
 test('blogs are returned as json', async () => {
     await api
@@ -15,6 +44,26 @@ test('check unique identifier', async ()=> {
 
     expect(res.body[0].id).toBeDefined()
 })
+
+// 4.10
+test('check if can create new blog', async ()=>{
+    const newTestBlog = {
+        title:"4.10 test",
+        author:"bill",
+        url:"www.google.com/test",
+        likes:100
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newTestBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    })
+
 
 
 afterAll(() => {
